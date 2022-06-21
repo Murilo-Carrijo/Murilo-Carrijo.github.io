1a168ec8085bda609d8c3695b98a3960fbf9e94c
murilo <murilocarrijoadm@gmail.com>
2022-06-21 02:06:42 -0300

    fix: refatorando css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,15 +27,20 @@ class Home extends Component {
           </div>
           <img className='profile-image' src={murilo} />
         </div>
-        <div className="card-description">
-          <h3>Um pouco a meu respeito</h3>
-          <p>Sou um profissional formado em administração de empresas que se apaixonou pela área de desenvolvimento de software, e que agora vive uma transição de carreira. Estudo desenvolvimento de software na Trybe e tenho como objetivo desenvolver minha carreira sob a supervisão e estímulo de uma empresa comprometida com a inovação.</p>
-          <p>Continue lendo sobre a minha história <Link to='/historia'>clicando aqui!</Link></p>
-        </div>
+        <div className="card-container">
+          <div className="card-description">
+            <h3 className="card-title" >Um pouco a meu respeito</h3>
+            <p>Sou um profissional formado em administração de empresas que se apaixonou pela área de desenvolvimento de software, e que agora vive uma transição de carreira.</p>
+            <p>Estudo desenvolvimento de software na Trybe e tenho como objetivo desenvolver minha carreira sob a supervisão e estímulo de uma empresa comprometida com a inovação.</p>
+            <button type="button" className="button-card-description"><Link to='/historia'>Saiba Mais</Link></button>
+          </div>
 
-        <div className="card-description">
-          <h3>Meus projetos</h3>
-          <p>Durante o meu curso na Trybe pude desenvolver projetos incríveis, que foram desafiadores e me propiciaram muitos aprendizados. <Link to='/projetos'>Clique aqui</Link> e confira os meus projetos</p>
+          <div className="card-description">
+            <h3 className="card-title">Meus projetos</h3>
+            <p>Durante o meu curso na Trybe pude desenvolver projetos incríveis, que foram desafiadores e me propiciaram muitos aprendizados.</p>
+            <p>Para desenvolver os projetos foram utilizadas diversas tecnoligias para poder desenvolver os projetos como HTML, CSS, JavaScript, React entre outras. </p>
+            <button type="button" className="button-card-description"><Link to='/projetos'>Saiba Mais</Link></button>
+          </div>
         </div>
       </div>
     );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,14 +15,14 @@ class Projects extends Component {
         <h1>Projetos</h1>
         { data.map((project) => (
           <div key={project.id} className='card-project'>
+            <a href={ project.link } target='_blank' rel="noreferrer noopener">
+              <img src={ project.image } alt={ project.name } />
+            </a>
             <div className='card-project-text'>
               <h2>{ project.name }</h2>
               <p>Tecnologias Utilizadas: {project.technology} </p>
               <p>Clique na imagem para acessar o repositório do GitHub</p>
             </div>
-            <a href={ project.link } target='_blank' rel="noreferrer noopener">
-              <img src={ project.image } alt={ project.name } />
-            </a>
           </div>
         ) ) }
       </div>
